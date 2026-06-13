6d24ca3bfee329f35b0529286518a395ab4771fe
Edward117i <ed_casa_123@hotmail.com>
2026-06-13 12:58:54 -0600

    feat: add year field to Car model and update database schema

--- my_first_project/my_first_app/models.py
+++ my_first_project/my_first_app/models.py
@@ -3,4 +3,4 @@ from django.db import models
 # Create your models here.
 class Car(models.Model):
     title = models.TextField(max_length=250)
-    
+    year = models.IntegerField(max_length=4, null=True)
