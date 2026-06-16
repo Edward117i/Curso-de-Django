c406c6a8e827942eda58769adb67d18a6d122554
Edward117i <ed_casa_123@hotmail.com>
2026-06-16 16:43:10 -0600

    feat: add Publisher, Author, Profile, and Book models with string representations

--- my_first_project/my_first_app/models.py
+++ my_first_project/my_first_app/models.py
@@ -8,3 +8,34 @@ class Car(models.Model):
 
     def __str__(self):
         return f"{self.title} - {self.year}"
+
+class Publisher(models.Model):
+    name = models.TextField(max_length=200)
+    address = models.TextField(max_length=200)
+
+    def __str__(self):
+        return f"{self.name} - {self.address}"
+
+class Author(models.Model):
+    name = models.TextField(max_length=200)
+    birth_date = models.DateField()
+
+    def __str__(self):
+        return f"{self.name} {self.birth_date}"
+
+class Profile (models.Model):
+    author = models.OneToOneField(Author, on_delete=models.CASCADE)
+    website = models.URLField(max_length=200)
+    biography = models.TextField(max_length=200)
+
+    def __str__(self):
+        return f"{self.author.name} {self.website} {self.biography}"
+
+class Book(models.Model):
+    title = models.TextField(max_length=200)
+    publication_date = models.DateField()
+    publisher = models.ForeignKey(Publisher, on_delete=models.CASCADE)
+    authors = models.ManyToManyField(Author, related_name='authors')
+
+    def __str__(self):
+        return f"{self.title} {self.publisher}"
